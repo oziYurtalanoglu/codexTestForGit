9c2edec1f278be406a9b6b76f11c75e8664c3266
Ozan Yurtalanoğlu <ozan.yurtalanoglu@gmail.com>
2026-02-19 11:02:48 +0300

    dev

---
+++ codexTestForGit/noteBackend/code/src/test/java/com/example/notebackend/CodeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.notebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
